7826108e6a57cc934cc324d0bdcefa6a346423ba
tolafad <adetola.fadahunsi@constant.co>
2018-02-25 16:21:24 +0000

    added tests

---
+++ src/test/java/GildedRoseTest.java
@@ -0,0 +1,61 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void testTheTruth() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testQualityDegradesBeforeSellIn() {
+        fail();
+    }
+
+    @Test
+    public void testQualityDegradesTwiceAsMuchAfterSellIn() {
+        fail();
+    }
+
+    @Test
+    public void testQualityIsNeverNegative() {
+        fail();
+    }
+
+    @Test
+    public void testAgedBrieIncreaseInQuality() {
+        fail();
+    }
+
+    @Test
+    public void testQualityIsNeverMoreThan50() {
+        fail();
+    }
+
+    @Test
+    public void testSulfurasNeverDecreasesInQuality() {
+        fail();
+    }
+
+    @Test
+    public void testBackstageIncreaseQualityBy2_When_10daysOrLess() {
+        fail();
+    }
+
+    @Test
+    public void testBackstageIncreaseQualityBy3_When_5daysOrLess() {
+        fail();
+    }
+    
+    @Test
+    public void testQualityIs0_When_concert() {
+        fail();
+    }
+    
+    @Test
+    public void testConjuredItemsDecreaseInQualityTwiceAsFast() {
+        fail();
+    }
+}
