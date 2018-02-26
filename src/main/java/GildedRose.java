1c34082e0577b13d20ab263ea4e09b74a10e3d48
tolafad <adetola.fadahunsi@constant.co>
2018-02-26 02:22:36 +0000

    refactored updateAnItem method

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public class GildedRose {
 
+    private static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+    private static final int MAX_QUALITY = 50;
     static final String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
     static final String ITEM_5_DEXTERITY_VEST = "+5 Dexterity Vest";
     static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
@@ -23,7 +25,7 @@ public class GildedRose {
         items.add(new Item(ELIXIR_OF_THE_MONGOOSE, 5, 7));
         items.add(new Item(SULFURAS_HAND_OF_RAGNAROS, 0, 80));
         items.add(new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
+        items.add(new Item(CONJURED_MANA_CAKE, 3, 6));
 
         updateQuality(items);
     }
@@ -35,42 +37,57 @@ public class GildedRose {
     }
 
     public static void updateAnItem(Item item) {
-        if ((!AGED_BRIE.equals(item.getName())) && !BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.getName())) {
-            decrementQuality(item);
-        } else {
-            incrementQuality(item);
-
-            if (BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.getName())) {
-                if (item.getSellIn() < 11) {
-                    incrementQuality(item);
-                }
-
-                if (item.getSellIn() < 6) {
-                    incrementQuality(item);
-                }
-            }
-        }
-
+        updateQuality(item);
         updateSellIn(item);
         updateQualityWhenSellInHasEnded(item);
     }
 
+    private static void updateQuality(Item item) {
+        if (isAgedBrie(item)) {
+            incrementQuality(item);            
+        }
+        else if (isBackStageToConcert(item)) {
+            incrementQuality(item);
+
+            if (item.getSellIn() < 11) {
+                incrementQuality(item);
+            }
+
+            if (item.getSellIn() < 6) {
+                incrementQuality(item);
+            }
+
+        } else {
+            decrementQuality(item);
+        }
+    }
+
+    private static boolean isBackStageToConcert(Item item) {
+        return BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.getName());
+    }
+
+    private static boolean isAgedBrie(Item item) {
+        return AGED_BRIE.equals(item.getName());
+    }
+
     private static void updateSellIn(Item item) {
-        if (!SULFURAS_HAND_OF_RAGNAROS.equals(item.getName())) {
+        if (!isSulfuras(item)) {
             item.setSellIn(item.getSellIn() - 1);
         }
     }
 
+    private static boolean isSulfuras(Item item) {
+        return SULFURAS_HAND_OF_RAGNAROS.equals(item.getName());
+    }
+
     private static void updateQualityWhenSellInHasEnded(Item item) {
-        if (hasSellEnded(item)) {
-            if (!AGED_BRIE.equals(item.getName())) {
-                if (!BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.getName())) {
-                    decrementQuality(item);
-                } else {
-                    item.setQuality(item.getQuality() - item.getQuality());
-                }
-            } else {
+        if (hasSellInEnded(item)) {
+            if (isAgedBrie(item))
                 incrementQuality(item);
+            else if (isBackStageToConcert(item)) {
+                item.setQuality(0);
+            } else {
+                decrementQuality(item);
             }
         }
     }
@@ -79,17 +96,19 @@ public class GildedRose {
         if (item.getQuality() < 50) {
             item.setQuality(item.getQuality() + 1);
         }
+        else
+            item.setQuality(MAX_QUALITY);
     }
 
     private static void decrementQuality(Item item) {
         if (item.getQuality() > 0) {
-            if (!SULFURAS_HAND_OF_RAGNAROS.equals(item.getName())) {
+            if (!isSulfuras(item)) {
                 item.setQuality(item.getQuality() - 1);
             }
         }
     }
 
-    private static boolean hasSellEnded(Item item) {
+    private static boolean hasSellInEnded(Item item) {
         return item.getSellIn() < 0;
     }
 
