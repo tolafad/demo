534c5ae4610d89c1fc55544900b4ab97fc381447
tolafad <adetola.fadahunsi@constant.co>
2018-02-26 01:03:12 +0000

    added tests

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,102 +1,84 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GildedRose {
 
-	private static List<Item> items = null;
+    static final String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
+    static final String ITEM_5_DEXTERITY_VEST = "+5 Dexterity Vest";
+    static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    static final String AGED_BRIE = "Aged Brie";
+    static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        List<Item> items = null;
+
         System.out.println("OMGHAI!");
-		
+
         items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item(ITEM_5_DEXTERITY_VEST, 10, 20));
+        items.add(new Item(AGED_BRIE, 2, 0));
+        items.add(new Item(ELIXIR_OF_THE_MONGOOSE, 5, 7));
+        items.add(new Item(SULFURAS_HAND_OF_RAGNAROS, 0, 80));
+        items.add(new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
 
-        updateQuality();
-}
+        updateQuality(items);
+    }
 
+    public static void updateQuality(List<Item> items) {
+        for (Item item : items) {
+            updateAnItem(item);
+        }
+    }
 
-	
-    public static void updateQuality()
-    {
-        for (int i = 0; i < items.size(); i++)
-        {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
+    public static void updateAnItem(Item item) {
+        if ((!AGED_BRIE.equals(item.getName())) && !BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.getName())) {
+            if (item.getQuality() > 0) {
+                if (!SULFURAS_HAND_OF_RAGNAROS.equals(item.getName())) {
+                    item.setQuality(item.getQuality() - 1);
                 }
             }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
+        } else {
+            if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
 
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
+                if (BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.getName())) {
+                    if (item.getSellIn() < 11) {
+                        if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
                         }
+                    }
 
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
+                    if (item.getSellIn() < 6) {
+                        if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
                         }
                     }
                 }
             }
+        }
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-            }
+        if (!SULFURAS_HAND_OF_RAGNAROS.equals(item.getName())) {
+            item.setSellIn(item.getSellIn() - 1);
+        }
 
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
+        if (item.getSellIn() < 0) {
+            if (!AGED_BRIE.equals(item.getName())) {
+                if (!BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT.equals(item.getName())) {
+                    if (item.getQuality() > 0) {
+                        if (!SULFURAS_HAND_OF_RAGNAROS.equals(item.getName())) {
+                            item.setQuality(item.getQuality() - 1);
                         }
                     }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
+                } else {
+                    item.setQuality(item.getQuality() - item.getQuality());
                 }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
-                    }
+            } else {
+                if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
                 }
             }
         }
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,61 +1,115 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class GildedRoseTest {
 
+    
     @Test
     public void testTheTruth() {
         assertTrue(true);
     }
 
     @Test
-    public void testQualityDegradesBeforeSellIn() {
-        fail();
+    public void testQualityDegradesByOneBeforeSellIn() {
+        Item item = new Item(GildedRose.ITEM_5_DEXTERITY_VEST, 10, 20);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
+        this.assertItem(GildedRose.ITEM_5_DEXTERITY_VEST, 9, 19, items.get(0));
     }
 
     @Test
     public void testQualityDegradesTwiceAsMuchAfterSellIn() {
-        fail();
+        Item item = new Item(GildedRose.ITEM_5_DEXTERITY_VEST, 0, 20);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
+        this.assertItem(GildedRose.ITEM_5_DEXTERITY_VEST, -1, 18, items.get(0));
+
     }
 
     @Test
     public void testQualityIsNeverNegative() {
-        fail();
+        Item item = new Item(GildedRose.ITEM_5_DEXTERITY_VEST, 10, 0);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
+        this.assertItem(GildedRose.ITEM_5_DEXTERITY_VEST, 9, 0, items.get(0));
     }
 
     @Test
     public void testAgedBrieIncreaseInQuality() {
-        fail();
+        Item item = new Item(GildedRose.AGED_BRIE, 2, 0);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
+        this.assertItem(GildedRose.AGED_BRIE, 1, 1, items.get(0));
+
     }
 
     @Test
     public void testQualityIsNeverMoreThan50() {
-        fail();
+        Item item = new Item(GildedRose.AGED_BRIE, 5, 50);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
+        this.assertItem(GildedRose.AGED_BRIE, 4, 50, items.get(0));
+
     }
 
     @Test
+    public void testQualityIsNeverMoreThan50_WhenQualityIsMoreThan50() {
+        Item item = new Item(GildedRose.AGED_BRIE, 5, 51);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
+        this.assertItem(GildedRose.AGED_BRIE, 4, 50, items.get(0));
+
+    }
+    @Test
     public void testSulfurasNeverDecreasesInQuality() {
-        fail();
+        Item item = new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 80);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
     }
 
     @Test
     public void testBackstageIncreaseQualityBy2_When_10daysOrLess() {
-        fail();
+        Item item = new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 15, 9);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
     }
 
     @Test
     public void testBackstageIncreaseQualityBy3_When_5daysOrLess() {
-        fail();
+        Item item = new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 15, 5);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
     }
     
     @Test
     public void testQualityIs0_When_concert() {
-        fail();
+        Item item = new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 0, 5);
+        List<Item> items = new ArrayList<Item>();
+        items.add(item);
+        GildedRose.updateQuality(items);
     }
     
     @Test
     public void testConjuredItemsDecreaseInQualityTwiceAsFast() {
         fail();
+    }
+    
+    private void assertItem(String name, int sellIn, int quality,Item item) {
+        assertEquals(name, item.name);
+        assertEquals("sellIn", sellIn, item.sellIn);
+        assertEquals("quality", quality,item.quality);
     }
 }
